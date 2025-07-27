09604d0e63aeb6f5148f439bfef9b480fe3be06f
vmuppidi0117 <vmuppidi@govst.edu>
2025-07-27 00:53:47 -0500

    CreateClassFilterViewModel

--- Models/ClassFilterViewModel.cs
+++ Models/ClassFilterViewModel.cs
@@ -9,7 +9,7 @@ namespace Project_Equinox.Models
 
         public List<Club> Clubs { get; set; } = new List<Club>();
         public List<ClassCategory> Categories { get; set; } = new List<ClassCategory>();
-        public List<EquinoxClass> EquinoxClasses { get; set; } = new List<EquinoxClass>();
+        public List<EquinoxClass> EquinoxClass { get; set; } = new List<EquinoxClass>();
         public List<int> BookClassIds { get; set; } = new List<int>();
         public Boolean IsClassBooked(int classId)
         {
