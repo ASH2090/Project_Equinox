f6c3eff44e0e5c693772cdc48a97f729ed91853f
Harsathabinav <harsathabinavas@gmail.com>
2025-07-29 15:36:57 -0500

    Phase2 Reviewed Changes

--- Models/EquinoxSession.cs
+++ Models/EquinoxSession.cs
@@ -9,12 +9,24 @@ namespace Project_Equinox.Models
         private const string FILTER_CLUB_ID = "FilterClubId";
         private const string FILTER_CATEGORY_ID = "FilterCategoryId";
 
+        // Generic helper methods for session operations
+        public static void SetObject<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T)! : JsonSerializer.Deserialize<T>(value)!;
+        }
+
         /// <summary>
         /// Saves the selected club filter value
         /// </summary>
         public static void SaveSelectedClub(this ISession session, int clubId)
         {
-            session.SetInt32(FILTER_CLUB_ID, clubId);
+            session.SetObject(FILTER_CLUB_ID, clubId);
         }
 
         /// <summary>
@@ -22,7 +34,7 @@ namespace Project_Equinox.Models
         /// </summary>
         public static void SaveSelectedClassCategory(this ISession session, int categoryId)
         {
-            session.SetInt32(FILTER_CATEGORY_ID, categoryId);
+            session.SetObject(FILTER_CATEGORY_ID, categoryId);
         }
 
         /// <summary>
@@ -39,7 +51,7 @@ namespace Project_Equinox.Models
         /// </summary>
         public static int GetSelectedClub(this ISession session)
         {
-            return session.GetInt32(FILTER_CLUB_ID) ?? 0;
+            return session.GetObject<int>(FILTER_CLUB_ID);
         }
 
         /// <summary>
@@ -47,7 +59,7 @@ namespace Project_Equinox.Models
         /// </summary>
         public static int GetSelectedClassCategory(this ISession session)
         {
-            return session.GetInt32(FILTER_CATEGORY_ID) ?? 0;
+            return session.GetObject<int>(FILTER_CATEGORY_ID);
         }
 
         /// <summary>
@@ -77,18 +89,6 @@ namespace Project_Equinox.Models
         public static bool HasActiveFilters(this ISession session)
         {
             return session.GetSelectedClub() != 0 || session.GetSelectedClassCategory() != 0;
-        }
-
-        // Generic helper methods for other session operations if needed
-        private static void SetObject<T>(this ISession session, string key, T value)
-        {
-            session.SetString(key, JsonSerializer.Serialize(value));
-        }
-
-        private static T GetObject<T>(this ISession session, string key)
-        {
-            var value = session.GetString(key);
-            return value == null ? default(T)! : JsonSerializer.Deserialize<T>(value)!;
         }
     }
 }
