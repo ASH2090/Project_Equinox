9ec8d1d19a675ce7a3a8c2ad52e3861584532c5d
Harsathabinav <harsathabinavas@gmail.com>
2025-08-03 19:22:36 -0500

    DataBase Correction

--- Migrations/20250804002217_AddImageToClassCategory.Designer.cs
+++ Migrations/20250804002217_AddImageToClassCategory.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project_Equinox.Models;
 
@@ -10,9 +11,11 @@ using Project_Equinox.Models;
 namespace Project_Equinox.Migrations
 {
     [DbContext(typeof(EquinoxContext))]
-    partial class EquinoxContextModelSnapshot : ModelSnapshot
+    [Migration("20250804002217_AddImageToClassCategory")]
+    partial class AddImageToClassCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
