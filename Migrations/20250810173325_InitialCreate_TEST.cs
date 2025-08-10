3225bf08a7366e5e6ed070f5d994012410599dba
Harsathabinav <harsathabinavas@gmail.com>
2025-08-10 12:33:44 -0500

    DB

---
+++ Migrations/20250810173325_InitialCreate_TEST.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project_Equinox.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_TEST : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
